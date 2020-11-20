508c75cffc89cfb85712e1b342dad261c52aee65
Sunshine <74633666+ChrisPark2@users.noreply.github.com>
2020-11-20 13:55:25 -0500

    Update coding_practice_2020_11_20.py

--- coding_practice_2020_11_20.py
+++ coding_practice_2020_11_20.py
@@ -12,3 +12,13 @@ length = int(input("what is the length of the rectangular prism in cm? "))
 width = int(input("What is the width of the rectangular prism in cm? "))
 height = int(input("What is the height of the rectangular prism in cm? "))
 print("The volume of the rectangular prism is", length * width * height, "cm squared")
+
+#4 Create a program that will ask the user if they just join a Google Meet and mute the teacher. If they enter yes say "That's probably not a good idea". If they say no, then output "Ok. Good.".
+print("Did you join a Google meet and mute the teacher?: ")
+yesorno = input("[A] Yes [B] No  ")
+
+if yesorno == "A":
+    print("WHAT ARE U DOING U CANT HEAR HIM UNMUTE DUMBDUMB")
+
+elif yesorno == "B":
+    print("fantastic now pay attention or ur become homeless")
